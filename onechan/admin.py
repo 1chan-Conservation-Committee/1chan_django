57087e306123e61591b13ac3b7b03d3da6c392c5
postman0 <postman0@users.noreply.github.com>
2016-01-21 18:00:26 +0300

    allow to manage homeboards through admin site

--- onechan/admin.py
+++ onechan/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Comment, Smiley
+from .models import Post, Category, Comment, Smiley, Homeboard
 
 
-admin.site.register([Post, Category, Comment, Smiley])
+admin.site.register([Post, Category, Comment, Smiley, Homeboard])
