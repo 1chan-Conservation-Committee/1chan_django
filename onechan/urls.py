1227f6ec411c894a6f7b21816e0d76b739219e11
postman0 <postman0@users.noreply.github.com>
2016-01-22 19:53:34 +0300

    changed news article urls to the more sane alternatives

--- onechan/urls.py
+++ onechan/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r"^news/all$", views.PostsListView.as_view(), {'posts_type': 'all'}, name='all_posts'),
     url(r"^news/hidden$", views.PostsListView.as_view(), {'posts_type': 'hidden'}, name='hidden_posts'),
 
-    url(r'^news/res/(?P<post_id>[0-9]+)$', views.show_post, name='show_post'),
+    url(r'^news/(?P<post_id>[0-9]+)$', views.show_post, name='show_post'),
     url(r'^news/add$', views.add_post, name='add_post'),
-    url(r'^news/res/(?P<post_id>[0-9]+)/rate$', views.rate_post, name='rate_post'),
-    url(r'^news/res/(?P<post_id>[0-9]+)/add_comment$', views.add_comment, name='add_comment'),
+    url(r'^news/(?P<post_id>[0-9]+)/rate$', views.rate_post, name='rate_post'),
+    url(r'^news/(?P<post_id>[0-9]+)/add_comment$', views.add_comment, name='add_comment'),
 ]
