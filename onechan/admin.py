51abbb3df11a9b485a7ddcd319f1d3c450b1a369
postman0 <postman0@users.noreply.github.com>
2016-02-11 16:03:29 +0300

    added approver functionality

--- onechan/admin.py
+++ onechan/admin.py
@@ -1,5 +1,23 @@
+from django import forms
 from django.contrib import admin
 from .models import Post, Category, Comment, Smiley, Homeboard
 
 
-admin.site.register([Post, Category, Comment, Smiley, Homeboard])
+admin.site.register([Category, Comment, Smiley, Homeboard])
+
+
+class PostApproverForm(forms.ModelForm):
+
+    class Meta:
+        fields = ['category', 'status', 'hide_reason', 'pinned', 'bumpable']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'title', 'status', 'category')
+    list_display_links = ('title',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        if not request.user.is_superuser:
+            kwargs['form'] = PostApproverForm
+        return super(PostAdmin, self).get_form(request, obj, **kwargs)
